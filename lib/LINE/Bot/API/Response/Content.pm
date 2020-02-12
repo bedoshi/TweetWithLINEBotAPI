8367c5e931f4bbfc8f04b97fbe757c6f81ba2b70
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-02-12 18:32:11 +0900

    removed local lib of line-bot-api.

--- lib/LINE/Bot/API/Response/Content.pm
+++
@@ -1,9 +0,0 @@
-package LINE::Bot::API::Response::Content;
-use strict;
-use warnings;
-use parent 'LINE::Bot::API::Response::Common';
-
-sub fh      { $_[0]->{fh} }
-sub headers { $_[0]->{headers} }
-
-1;
--- lib/LINE/Bot/API/Response/TotalUsage.pm
+++
@@ -1,8 +0,0 @@
-package LINE::Bot::API::Response::TotalUsage;
-use strict;
-use warnings;
-use parent 'LINE::Bot::API::Response::Common';
-
-sub total_usage { $_[0]->{totalUsage} }
-
-1;
